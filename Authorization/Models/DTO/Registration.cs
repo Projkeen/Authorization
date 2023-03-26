27d8eaa57bc312ffbe87e8e262d2ca60f565c9b8
Projkeen Theone <projkeen@outlook.com>
2023-03-26 10:58:55 +0300

    Добавьте файлы проекта.

---
+++ Authorization/Models/DTO/Registration.cs
@@ -0,0 +1,9 @@
+﻿namespace Authorization.Models.DTO
+{
+    public class Registration
+    {       
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
